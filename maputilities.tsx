6affbcf876a76ab7c5d6f60d7271fa306448e966
Dennis Lindemann <git@magierdinge.de>
2020-12-10 20:42:22 +0100

    bäm

--- maputilities.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="maputilities" tilewidth="32" tileheight="32" tilecount="9" columns="9">
- <image source="tiles/structures/mapUtilities.png" width="288" height="32"/>
-</tileset>
